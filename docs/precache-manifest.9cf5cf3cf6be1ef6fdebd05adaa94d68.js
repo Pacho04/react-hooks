a26331e5bdca3d64202e5357b79e323f740db5bb
ismael-creator <ismabarri41@gmail.com>
2020-09-14 21:25:55 -0300

    Docs

---
+++ docs/precache-manifest.9cf5cf3cf6be1ef6fdebd05adaa94d68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbcf1a01b4f6f75bddcc5195c93b626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08b3bd97a67ffe165c8b",
+    "url": "/static/css/main.fa1226af.chunk.css"
+  },
+  {
+    "revision": "9570073f3c7294322850",
+    "url": "/static/js/2.3810d313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3810d313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b3bd97a67ffe165c8b",
+    "url": "/static/js/main.9ee8f3cc.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
